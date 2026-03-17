4db894ab1b8884fb62a73c6f7605012dfede8c47
CLBradley <corylbradley@gmail.com>
2026-03-16 20:02:18 -0400

    Initial Commit

---
+++ CLBradley_App/CLBradley_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLBradley_AppApp.swift
+//  CLBradley_App
+//
+//  Created by Cory Bradley on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CLBradley_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
